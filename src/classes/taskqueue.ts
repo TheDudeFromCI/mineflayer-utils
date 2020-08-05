b6b7792a1147de4faaf976820f9243d338b8159f
TheDudeFromCI <thedudefromci@gmail.com>
2020-08-05 05:16:36 -0700

    Added task queue utility.

---
+++ src/classes/taskqueue.ts
@@ -0,0 +1,94 @@
+import { Callback } from "../types";
+
+export type Task = (cb: Callback) => void;
+export type SyncTask = () => void;
+
+/**
+ * A simple utility class for queuing up a series of async tasks to execute.
+ */
+export class TaskQueue
+{
+    private tasks: Task[] = [];
+
+    /**
+     * If true, the task list will stop executing if one of the tasks throws an error.
+     */
+    readonly stopOnError: boolean = true;
+
+    /**
+     * Adds a new async task to this queue. The provided callback should be executed when
+     * the async task is complete.
+     * 
+     * @param task - The async task to add.
+     */
+    add(task: Task): void
+    {
+        this.tasks.push(task);
+    }
+
+    /**
+     * Adds a synchronous task toi this queue.
+     * 
+     * @param task - The sync task to add.
+     */
+    addSync(task: SyncTask): void
+    {
+        this.add(cb =>
+        {
+            try
+            {
+                task();
+            }
+            catch (err)
+            {
+                cb(err);
+                return;
+            }
+        });
+    }
+
+    /**
+     * Runs all tasks currently in this queue and empties the queue.
+     * 
+     * @param cb - The optional callback to be executed when all tasks in this queue have
+     * finished executing.
+     */
+    runAll(cb?: Callback): void
+    {
+        const taskList = this.tasks;
+        this.tasks = [];
+
+        let index = -1;
+        const runNext = () =>
+        {
+            index++;
+            if (index >= taskList.length)
+            {
+                if (cb) cb();
+                return;
+            }
+
+            try
+            {
+                taskList[index](err =>
+                {
+                    if (err)
+                    {
+                        if (cb) cb(err);
+
+                        if (this.stopOnError)
+                            return;
+                    }
+
+                    runNext();
+                });
+            }
+            catch (err)
+            {
+                if (cb) cb(err);
+            }
+        };
+
+        runNext();
+    }
+}
